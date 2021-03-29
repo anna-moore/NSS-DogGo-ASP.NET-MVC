971e8c335f63e40d359868547f55572075c692e1
anna-moore <ammoore434@gmail.com>
2021-03-29 13:51:37 -0500

    update view of owner details

--- DogGo/Controllers/OwnersController.cs
+++ DogGo/Controllers/OwnersController.cs
@@ -12,11 +12,18 @@ namespace DogGo.Controllers
     public class OwnersController : Controller
     {
         private readonly IOwnerRepository _ownerRepo;
+        private readonly IDogRepository _dogRepo;
+        private readonly IWalkerRepository _walkerRepo;
 
         // ASP.NET will give us an instance of our OWNER Repository. This is called "Dependency Injection"
-        public OwnersController(IOwnerRepository ownerRepository)
+        public OwnersController(
+            IOwnerRepository ownerRepository,
+            IDogRepository dogRepository,
+            IWalkerRepository walkerRepository)
         {
             _ownerRepo = ownerRepository;
+            _dogRepo = dogRepository;
+            _walkerRepo = walkerRepository;
         }
 
         // GET: Owners
